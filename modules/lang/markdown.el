a5cd02d8271327ff601ca4266bee59a05ea26221
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 12:50:04 -0400

    feat: markdown mode

---
+++ modules/lang/markdown.el
@@ -0,0 +1,11 @@
+;;; lang/markdown ---  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package markdown-mode
+  :straight nil)
+
+(provide 'lang/markdown)
+;;; markdown.el ends here
