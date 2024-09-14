af55b627dec853cfbe8d93f7a9600f8d6811de93
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 12:41:54 -0400

    wip: agda polymode

--- modules/editor/polymode.el
+++ modules/editor/polymode.el
@@ -4,7 +4,26 @@
 
 ;;; Code:
 
-(use-package polymode)
+(use-package polymode
+  :config
+  (define-hostmode poly-markdown-hostmode
+    :mode 'markdown-mode)
+
+  (define-innermode poly-markdown-agda-innermode
+    :mode 'agda2-mode
+    :head-matcher "^[\t]*```agda\n"
+    :tail-matcher "^[\t]*```\n"
+    :head-mode 'host
+    :tail-mode 'host)
+
+  (define-polymode poly-markdown-mode
+    :hostmode 'poly-markdown-hostmode
+    :innermodes '(poly-markdown-agda-innermode))
+
+  )
+
+;; Experimenting with `polymode' and literate Agda.
+
 
 (provide 'editor/polymode)
 ;;; polymode.el ends here
