76f65141840e1421ae7eb15e96e1af00133ed891
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 14:06:24 -0400

    fix: seriously clean up agda mode

--- modules/lang/agda.el
+++ modules/lang/agda.el
@@ -1,67 +1,90 @@
+
 ;;; lang/agda ---  -*- lexical-binding: t; -*-
 
 ;;; Commentary:
+;; `agda2-mode' is a bit tricky to manage.  The version of Agda in use is
+;; tied to a specific version of `agda2-mode'; this can cause problems when
+;; working with multiple versions of Agda.  This gets particularly bad when
+;; `nix' gets involved, as the `agda2-mode' files we need will be located
+;; inside of the `nix' store.
+;;
+;; Luckily, `agda2-mode' comes with a solution for this in the form of
+;; `agda2-set-program-version'.  This function unloads and reloads all of
+;; the relevant `agda2-mode' files from Emacs with the appropriate version.
+;; This even works with `nix', provided that the `agda-mode' binary is on
+;; the PATH.
+;;
+;; However, this unload/reload process interacts rather poorly with `use-package',
+;; as it will completely clobber any customization.  To avoid this problem, we place
+;; all customization in an `with-eval-after-load' form inside of the `:init' block:
+;; this ensures that any further reloads will cause the customization code to run again.
+
 
 ;;; Code:
 (require 'core/tweaks)
 (require 'core/path)
 
-;; We need to load `agda2.el' from the nix store, as we need to use the version of Agda that
-;; comes with the 1Lab.
-(defconst agda-nix-path "/nix/store/qkcf5gjs2g5rzjc63jf4qhz5n6kyknjd-Agda-2.8.0-data/share/ghc-9.4.6/x86_64-osx-ghc-9.4.6/Agda-2.8.0/emacs-mode/agda2.el")
-(add-to-path "/nix/store/k1zz4baymxqaqxfw9s2281wk4pksj999-ghc-9.4.6-with-packages/bin")
+(require 'editor/snippets)
 
-(load-file agda-nix-path)
+(create-file-template ".*.agda$" "agda-template" 'agda2-mode)
+(create-file-template ".*.lagda.md$" "lagda-template" 'agda2-mode)
 
 (use-package agda2-mode
-  :straight nil
-  :mode "\\.lagda\\.md\\'"
-  :custom
-  (require 'meow)
-  (agda2-program-name "/nix/store/k1zz4baymxqaqxfw9s2281wk4pksj999-ghc-9.4.6-with-packages/bin/agda")
-
-  :hook
-  (agda2-mode . display-fill-column-indicator-mode)
-  :config
-  (create-file-template ".*.agda$" "agda-template" 'agda2-mode)
-  (create-file-template ".*.lagda.md$" "lagda-template" 'agda2-mode)
-  (set-fill-column 72)
-
-  (add-to-list 'compilation-error-regexp-alist-alist
-	       '(agda "^[\s-]*\\(?:at \\)?\\(.*\\):\\([0-9]+\\),\\([0-9]+\\)-\\([0-9]+\\)$" 1 2 (3 . 4) 2 1))
-  (add-to-list 'compilation-error-regexp-alist 'agda))
+  :preface
+  (defun agda2-mode-display-fill-column ()
+    "Enable `display-fill-column-indicator-mode' and set the fill column to 72."
+    (display-fill-column-indicator-mode 1)
+    (setq-local fill-column 72))
+  :init
+  ;; See COMMENTARY above.
+  (with-eval-after-load 'agda2-mode
+    (add-hook 'agda2-mode-hook #'agda2-mode-display-fill-column)
+    (add-to-list 'auto-mode-alist '("\\.lagda\\.md\\'" . agda2-mode))
+    (add-to-list 'compilation-error-regexp-alist-alist
+		 '(agda "^[\s-]*\\(?:at \\)?\\(.*\\):\\([0-9]+\\),\\([0-9]+\\)-\\([0-9]+\\)$" 1 2 (3 . 4) 2 1))
+    (add-to-list 'compilation-error-regexp-alist 'agda)))
 
 (use-package agda-input
   :straight nil
-  :defer nil
+  ;; We want to be able to call `set-input-method' with `Agda'
+  ;; without opening an agda file, so we force `agda-input' to load
+  ;; immediately.
+  :demand t
+  :functions agda-input-add-translations
+  :init
+  ;; See COMMENTARY above.
+  (with-eval-after-load 'agda-input
+    (agda-input-add-translations
+     '(("hom" . "⇒")
+       ("lam" . "λ")
+       ("lam-" . "ƛ")
+       ("iso" . "≅")
+       ("embed" . "↪")
+       ("mono" . "↣")
+       ("epi" . "↠")
+       ("tail" . "⤚")
+       ("nat" . "ℕ")
+       ("int" . "ℤ")
+       ("alpha" . "α")
+       ("beta" . "β")
+       ("gamma" . "γ")
+       ("yo" . "よ")
+       ("inv" . "⁻¹")
+       ("monus" . "∸")
+       ("U" . "⋃")
+       ("lsq" . "⊑")
+       ("cc" . "𝒞")
+       ("dd" . "𝒟")
+       ("uu" . "⇑")
+       ("wl" . "◀")
+       ("wr" . "▶")
+       ("cw" . "○")
+       ("cf" . "●"))))
   :config
-  (require 'agda-input)
-  (agda-input-add-translations
-   '(("hom" . "⇒")
-     ("lam" . "λ")
-     ("lam-" . "ƛ")
-     ("iso" . "≅")
-     ("embed" . "↪")
-     ("mono" . "↣")
-     ("epi" . "↠")
-     ("tail" . "⤚")
-     ("nat" . "ℕ")
-     ("int" . "ℤ")
-     ("alpha" . "α")
-     ("beta" . "β")
-     ("gamma" . "γ")
-     ("yo" . "よ")
-     ("inv" . "⁻¹")
-     ("monus" . "∸")
-     ("U" . "⋃")
-     ("lsq" . "⊑")
-     ("cc" . "𝒞")
-     ("dd" . "𝒟")
-     ("uu" . "⇑")
-     ("wl" . "◀")
-     ("wr" . "▶")
-     ("cw" . "○")
-     ("cf" . "●"))))
+  ;; We have to explicitly `require' here to ensure that the input method is available.
+  (require 'agda-input))
+
+
 
 (provide 'lang/agda)
 ;;; agda.el ends here
