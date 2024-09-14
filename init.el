98610b469c3d200646f33d1b0978c79cf8bcd30d
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 12:52:07 -0400

    chore: update init.el

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 ;; Core editor functionality.
 (require 'core/straight)
 (require 'core/path)
+(require 'core/custom)
 (require 'core/tweaks)
 (require 'core/meow)
 (require 'core/help)
@@ -18,43 +19,30 @@
 (require 'editor/company)
 (require 'editor/compilation)
 (require 'editor/flycheck)
+(require 'editor/spelling)
 (require 'editor/lsp)
+(require 'editor/imenu)
 (require 'editor/projectile)
 (require 'editor/snippets)
-(require 'editor/polymode)
+;; (require 'editor/polymode)
 
 (require 'lang/agda)
 (require 'lang/elisp)
 (require 'lang/latex)
 (require 'lang/lean)
-(require 'lang/narya)
+(require 'lang/haskell)
+;; (require 'lang/narya)
 (require 'lang/nix)
 (require 'lang/ocaml)
 (require 'lang/prog)
 
 (require 'tools/magit)
+(require 'tools/direnv)
+(require 'tools/org)
 (require 'tools/vterm)
 
 (require 'theme/modus)
 (require 'theme/tweaks)
-
-;; Theme
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("b29ba9bfdb34d71ecf3322951425a73d825fb2c002434282d2e0e8c44fce8185"
-     default))
- '(safe-local-variable-values
-   '((projectile-project-compilation-cmd . "1lab-shake all -j"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (provide 'init)
 ;;; init.el ends here
