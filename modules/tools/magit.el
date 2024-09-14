bc68cdcfd9177631df1268c083e846e59464f0b7
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 12:39:00 -0400

    feat: better binds for magit

--- modules/tools/magit.el
+++ modules/tools/magit.el
@@ -7,19 +7,28 @@
 ;;; Code:
 (require 'core/meow)
 
+;; Set up a `git' menu for `meow'.
+(defconst meow-git-keymap (define-keymap))
+(meow-define-keys 'leader `("g" . ("git" . ,meow-git-keymap)))
+
 (use-package magit
   :commands magit-status magit-dispatch magit-file-dispatch
   :bind
   (:map magit-mode-map
 	("x" . magit-discard))
-  (:map meow-leader-keymap
+  (:map meow-git-keymap
 	("g" . magit-status)))
 
 (use-package git-timemachine
   :commands git-timemachine
   :bind
-  (:map meow-leader-keymap
-	("G" . git-timemachine)))
+  (:map meow-git-keymap
+	("t" . git-timemachine))
+  ;; `git-timemachine' fights with `meow' over keybindings,
+  ;; so we replace all the problematic bindings.
+  (:map git-timemachine-mode-map
+	("N" . git-timemachine-show-next-revision)
+	("P" . git-timemachine-show-previous-revision)))
 
 (provide 'tools/magit)
 ;;; magit.el ends here
