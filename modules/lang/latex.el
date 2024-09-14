d0a7dc6f82303c030f7f45954a0af08ec67d83db
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 12:40:21 -0400

    feat: spell-checking improvements for LaTeX

--- modules/lang/latex.el
+++ modules/lang/latex.el
@@ -3,10 +3,28 @@
 ;;; Commentary:
 
 ;;; Code:
+(require 'editor/spelling)
 (require 'core/path)
 (require 'info)
 
-(use-package auctex)
+(use-package auctex
+  :custom
+  (font-latex-user-keyword-classes
+   '(("citations" (("cite" "{") ("citep" "{") ("citet" "{")) font-lock-constant-face command)))
+  (LaTeX-verbatim-environments
+   '("code" "verbatim" "verbatim*" "filecontents" "filecontents*")))
+
+;; Set up `spell-fu', and exclude some faces from spellchecking.
+(use-package spell-fu
+  :after auctex
+  :preface
+  (require 'spell-fu)
+  (defun spell-fu-LaTeX-hook ()
+    (spell-fu-mode 1)
+    (push 'font-latex-sedate-face spell-fu-faces-exclude)
+    (push 'font-lock-constant-face spell-fu-faces-exclude)
+    (push 'font-latex-verbatim-face spell-fu-faces-exclude))
+  :hook (LaTeX-mode . spell-fu-LaTeX-hook))
 
 (use-package reftex
   :straight nil
@@ -27,10 +45,9 @@
   (LaTeX-mode . (company-autex-macros company-auctex-symbols company-auctex-environments)))
 
 (use-package cdlatex
-  :hook (LaTeX-mode . turn-on-cdlatex))
-
-;; Make TeXLive manuals available in `info', and also add all binaries to the path.
-
+  :hook (LaTeX-mode . turn-on-cdlatex)
+  :custom
+  (cdlatex-sub-super-scripts-outside-math-mode nil))
 
 (provide 'lang/latex)
 ;;; latex.el ends here
