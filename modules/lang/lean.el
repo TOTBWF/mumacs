a0e2e6fa353f93b9b792957c998779ec3b1b7637
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 12:39:46 -0400

    feat: lean polymode

--- modules/lang/lean.el
+++ modules/lang/lean.el
@@ -5,13 +5,26 @@
 ;;; Code:
 
 (use-package lean4-mode
-  :requires polymode
+  :requires polymode markdown-mode
   :straight (lean4-mode
 	     :type git
 	     :host github
 	     :repo "leanprover/lean4-mode"
 	     :files ("*.el" "data"))
-  :mode "\\.lean\\'")
+  :mode "\\.lean\\'"
+  :config
+  ;; Set up a `polymode' that enables `markdown-mode' in comments.
+  (define-hostmode poly-lean4-hostmode
+    :mode 'lean4-mode)
+  (define-innermode poly-lean4-markdown-innermode
+    :mode 'markdown-mode
+    :head-matcher "/-[-!]"
+    :tail-matcher "-/"
+    :head-mode 'host
+    :tail-mode 'host)
+  (define-polymode poly-lean4-mode
+    :hostmode 'poly-lean4-hostmode
+    :innermodes '(poly-lean4-markdown-innermode)))
 
 (provide 'lang/lean)
 ;;; lean.el ends here
