28f1808fe046c3662b6a775fc212e8697e92407c
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 12:51:30 -0400

    feat: rust mode

---
+++ modules/lang/rust.el
@@ -0,0 +1,13 @@
+;;; lang/rust ---  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+(require 'core/path)
+
+(add-to-path "~/.cargo/bin")
+
+(use-package rust-mode)
+
+(provide 'lang/rust)
+;;; rust.el ends here
