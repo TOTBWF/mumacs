481161d0c9942437437e41a74fc2a0b468016d7c
Reed Mullanix <reedmullanix@gmail.com>
2024-09-14 12:49:31 -0400

    feat: add `envrc' module

---
+++ modules/tools/direnv.el
@@ -0,0 +1,11 @@
+;;; tools/direnv ---  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+(use-package envrc
+  :hook
+  (after-init . envrc-global-mode))
+
+(provide 'tools/direnv)
+;;; direnv.el ends here
