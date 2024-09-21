af5cc8a4dbbcf8a3bfc2d5f24916b43fac75f4e7
Reed Mullanix <reedmullanix@gmail.com>
2024-09-21 15:42:44 -0400

    fix: repair agda template

--- modules/editor/project.el
+++ modules/editor/project.el
@@ -14,7 +14,12 @@
   ;; We need to place the entire prefix map into the meow leader keymap,
   ;; which is not an operations supported by `:bind-keymap'. Luckily loading
   ;; `project' is pretty snappy, so we don't need to stress too hard about deferring.
-  (meow-define-keys 'leader `("p" . ("project" . ,project-prefix-map))))
+  (meow-define-keys 'leader `("p" . ("project" . ,project-prefix-map)))
+
+  ;; Some useful helper functions.
+  (defun project-make-relative-to-root (filename &optional project)
+    "Convert FILENAME to be relative to the root of a `project.el' PROJECT.  Defaults to `project-current'"
+    (file-relative-name filename (project-root (or project (project-current))))))
 
 (provide 'editor/project)
 ;;; project.el ends here
