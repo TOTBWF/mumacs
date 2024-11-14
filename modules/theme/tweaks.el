146769cb6f0b08f1795030e6df793417942781a2
Reed Mullanix <reedmullanix@gmail.com>
2024-11-14 11:02:50 -0500

    theme: per-frame modeline via tab-bar

--- modules/theme/tweaks.el
+++ modules/theme/tweaks.el
@@ -3,7 +3,6 @@
 ;;; Commentary:
 
 ;;; Code:
-(require 'time)
 
 ;; Add a bit of padding around internal borders.
 (add-to-list 'default-frame-alist '(internal-border-width . 48))
@@ -11,12 +10,31 @@
 ;; Pragmata Pro, 14 pt font as default font.
 (set-face-attribute 'default nil :family "PragmataPro Mono" :height 140)
 
-;; Display battery in the modeline.
-(display-battery-mode 1)
+(use-package battery
+  :straight nil
+  :demand t
+  :config
+  (display-battery-mode 1))
 
-;; Display time on the modeline, but get rid
-(setq display-time-default-load-average nil)
-(display-time-mode 1)
+(use-package time
+  :straight nil
+  :demand t
+  :custom
+  (display-time-default-load-average nil)
+  :config
+  (display-time-mode 1))
+
+(use-package tab-bar
+  :straight nil
+  :demand t
+  :custom
+  (tab-bar-format '(tab-bar-format-history tab-bar-format-global))
+  :config
+  (tab-bar-mode 1))
+
+
+
+
 
 (provide 'theme/tweaks)
 ;;; tweaks.el ends here
