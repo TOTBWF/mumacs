424b212c535a3b2214cb0c28cc8853d071a93e21
Reed Mullanix <reedmullanix@gmail.com>
2024-12-16 13:35:06 -0800

    org: org tweaks

--- modules/tools/org.el
+++ modules/tools/org.el
@@ -24,10 +24,10 @@
   (defun resize-org-latex-overlays ()
     "Resize all org latex previews in the current buffer."
     (cl-loop for o in (car (overlay-lists))
-	     if (eq (overlay-get o 'org-overlay-type) 'org-latex-overlay)
-	     do (plist-put (cdr (overlay-get o 'display))
-			   :scale (expt text-scale-mode-step
-					text-scale-mode-amount))))
+             if (eq (overlay-get o 'org-overlay-type) 'org-latex-overlay)
+             do (plist-put (cdr (overlay-get o 'display))
+                           :scale (expt text-scale-mode-step
+                                        text-scale-mode-amount))))
   (defun org-mode-add-hooks ()
     "Hook for adding further minor-mode hooks when we are in `org-mode'."
     (add-hook 'text-scale-mode-hook #'resize-org-latex-overlays nil t))
@@ -40,6 +40,8 @@
   org-entry-blocked-p
   org-entry-is-done-p
   org-clocking-p
+  :diminish
+  org-cdlatex-mode
   :hook
   (org-mode . org-cdlatex-mode)
   (org-mode . org-mode-add-hooks)
@@ -57,24 +59,7 @@
      ("STOP" . font-lock-comment-face)))
   (org-log-done 'time)
   ;; Agenda settings
-  (org-agenda-custom-commands
-   '(("n" "Agenda and all tasks"
-      ((agenda ""
-	       ((org-agenda-skip-function '(org-agend-skip-entry-if-blocked-or-done))))
-       (tags-todo "+category={task}+todo={TODO\\|OPEN}-blocked={t}"
-		  ((org-agenda-sorting-strategy
-		    '(priority-down todo-state-down))
-		   (org-agenda-overriding-header
-		    "Active tasks")))
-       (tags-todo "+category={task}+todo={TODO\\|OPEN}+blocked={t}"
-		  ((org-agenda-sorting-strategy
-		    '(priority-down todo-state-down))
-		   (org-agenda-overriding-header
-		    "Blocked tasks")))
-       (tags-todo "+category={task}+todo={WAIT}"
-		  ((org-agenda-overriding-header
-		    "Blockers")))))))
-  (org-agenda-use-time-grid nil)
+
   ;; LaTeX settings
   ;; Add `mlmodern' as our font, and add the `microtype' package.
   ;; We modify this instead of `org-latex-packages-alist' to ensure
@@ -108,22 +93,16 @@
   ;; Also take the time to bump up the scale.
   (org-format-latex-options
    '(:foreground "White" :background nil :scale 1.65 :html-foreground
-		 "Black" :html-background "Black" :html-scale 1.0
-		 :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
+                 "Black" :html-background "Black" :html-scale 1.0
+                 :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
   ;; Highlight LaTeX snippets in org buffers.
-  (org-highlight-latex-and-related '(latex))
+  (org-highlight-latex-and-related '(native))
   ;; Org babel
   (org-babel-load-languages
    '((emacs-lisp . t)
      (shell . t)
      (haskell . t)))
   :config
-  (defun org-agend-skip-entry-if-blocked-or-done ()
-    "Skip all `org-agenda' entries that are either blocked or marked done."
-    (and (or (org-entry-blocked-p)
-	     (org-entry-is-done-p))
-	 (org-entry-end-position)))
-
   (defconst org-no-clock-mode-line-string
     (propertize "[No Clock] " 'face 'org-mode-line-no-clock))
 
@@ -141,6 +120,53 @@
 
   (advice-add 'org-clock-out :after #'org-clock-out-mode-line-advice)
   (advice-add 'org-clock-in :after #'org-clock-in-mode-line-advice))
+
+(use-package org-agenda
+  :straight nil
+  :demand t
+  :after org
+  :custom
+  (org-agenda-use-time-grid nil)
+  (org-agenda-custom-commands
+   '(("n" "Agenda and all tasks"
+      ((agenda ""
+               ((org-agenda-skip-function '(org-agend-skip-entry-if-blocked-or-done))))
+       (tags-todo "+category={task}+todo={TODO\\|OPEN}-blocked={t}-borceux"
+                  ((org-agenda-sorting-strategy
+                    '(priority-down todo-state-down))
+                   (org-agenda-overriding-header
+                    "Active tasks")))
+       (tags-todo "+category={task}+todo={TODO\\|OPEN}+blocked={t}-borceux"
+                  ((org-agenda-sorting-strategy
+                    '(priority-down todo-state-down))
+                   (org-agenda-overriding-header
+                    "Blocked tasks")))
+       (tags-todo "+category={task}+todo={WAIT}-borceux"
+                  ((org-agenda-overriding-header
+                    "Blockers")))))
+     ("b" "Borceux"
+      ((tags-todo "+borceux+example+todo={TODO\\|OPEN}"
+                  ((org-agenda-overriding-header "Examples")
+                   (org-agenda-hide-tags-regexp "example")
+                   (org-agenda-show-inherited-tags nil)))
+       (tags-todo "+borceux+definition+todo={TODO\\|OPEN}"
+                  ((org-agenda-overriding-header "Definitions")
+                   (org-agenda-hide-tags-regexp "definition")
+                   (org-agenda-show-inherited-tags nil)))
+       (tags-todo "+borceux+proposition+todo={TODO\\|OPEN}"
+                  ((org-agenda-overriding-header "Propositions")
+                   (org-agenda-hide-tags-regexp "proposition")
+                   (org-agenda-show-inherited-tags nil)))
+       (tags-todo "+borceux+exercise+todo={TODO\\|OPEN}"
+                  ((org-agenda-overriding-header "Exercises")
+                   (org-agenda-hide-tags-regexp "exercise")
+                   (org-agenda-show-inherited-tags nil)))))))
+  :config
+  (defun org-agend-skip-entry-if-blocked-or-done ()
+    "Skip all `org-agenda' entries that are either blocked or marked done."
+    (and (or (org-entry-blocked-p)
+             (org-entry-is-done-p))
+         (org-entry-end-position))))
 
 ;; `org-timeblock' lets get a better daily view.
 (use-package org-timeblock
@@ -169,14 +195,35 @@
 
 ;; Zotero link integration
 (use-package zotxt
-  :diminish org-zotxt-mode
+  :diminish
+  org-zotxt-mode
   :hook (org-mode . org-zotxt-mode))
 
 (use-package org-roam
   :after org
+  :demand t
   :functions
   org-roam-db-autosync-mode
   org-roam-node-find
+  org-roam-db-query
+  :preface
+  (defun org-roam-node-note-p (node)
+    "Return `nil' if a node is a task, and `t' otherwise"
+    (not (string-equal-ignore-case (org-roam-node-category node) "task")))
+
+  (defun org-roam-node-task-p (node)
+    "Return `nil' if a node is a task, and `t' otherwise"
+    (string-equal-ignore-case (org-roam-node-category node) "task"))
+
+  (defun org-roam-note-find ()
+    "Find and open an Org-roam node that is not a task."
+    (interactive current-prefix-arg)
+    (org-roam-node-find nil nil 'org-roam-node-note-p))
+
+  (defun org-roam-task-find ()
+    "Find and open an Org-roam node that is a task."
+    (interactive current-prefix-arg)
+    (org-roam-node-find nil nil 'org-roam-node-task-p))
   :config
   ;; Keep the `org-roam' session synchronized.
   (org-roam-db-autosync-mode 1)
@@ -197,27 +244,20 @@
      ("p" "person" plain "%?" :target
       (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: person\n* ${title}")
       :unnarrowed t)))
-  ;; HACK: This will get quite slow after a while; should use something smarter as detailed
-  ;; in https://d12frosted.io/posts/2021-01-16-task-management-with-roam-vol5.html
   (org-agenda-files '("~/Documents/Notes/"))
-  :config
-  (defun org-roam-node-note-p (node)
-    "Return `nil' if a node is a task, and `t' otherwise"
-    (not (string-equal-ignore-case (org-roam-node-category node) "task")))
-
-  (defun org-roam-note-find ()
-    "Find and open an Org-roam node that is not a task."
-    (interactive current-prefix-arg)
-    (org-roam-node-find nil nil 'org-roam-node-note-p))
   :bind
   (:map meow-org-keymap
-	("a" . org-agenda)
-	("o" . org-roam-note-find)
-	("c" . org-roam-capture)
-	("l" . org-store-link)
-	("i" . org-roam-node-insert)
-	("d" . org-roam-dailies-goto-today)
-	("y" . org-roam-dailies-goto-yesterday)))
+        ("a" . org-agenda)
+        ("o" . org-roam-note-find)
+        ("t" . org-roam-task-find)
+        ("c" . org-roam-capture)
+        ("l" . org-store-link)
+        ("x j" . org-clock-goto)
+        ("x o" . org-clock-out)
+        ("x i" . org-clock-in-last)
+        ("i" . org-roam-node-insert)
+        ("d" . org-roam-dailies-goto-today)
+        ("y" . org-roam-dailies-goto-yesterday)))
 
 (use-package org-fancy-priorities
   :after org
@@ -234,14 +274,15 @@
      (?C . "☕")
      (?D . "🧊"))))
 
-(use-package org-upcoming-modeline
-  :after org
-  ;; FIXME: There are byte-compiler errors with org-ql.
-  :no-require t
-  :demand t
-  :commands org-upcoming-modeline-mode
-  :config
-  (org-upcoming-modeline-mode 1))
+;; TOO SLOW
+;; (use-package org-upcoming-modeline
+;;   :after org
+;;   ;; FIXME: There are byte-compiler errors with org-ql.
+;;   :no-require t
+;;   :demand t
+;;   :commands org-upcoming-modeline-mode
+;;   :config
+;;   (org-upcoming-modeline-mode 1))
 
 (provide 'tools/org)
 ;;; org.el ends here
