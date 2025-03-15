b2b793b527017206488484384d9b486a635ac132
Reed Mullanix <reedmullanix@gmail.com>
2025-03-15 10:05:22 -0400

    feat: disproject.el

--- modules/editor/project.el
+++ modules/editor/project.el
@@ -10,16 +10,27 @@
   ;; Don't bother defering; gets quite fiddly with autoloading,
   ;; and benchmarking shows that this is fast to load.
   :demand t
+  :custom
+  (project-vc-extra-root-markers '(".project.el" ".projectile" "cabal.project" "cabal.project.local"))
   :config
   ;; We need to place the entire prefix map into the meow leader keymap,
   ;; which is not an operations supported by `:bind-keymap'. Luckily loading
   ;; `project' is pretty snappy, so we don't need to stress too hard about deferring.
-  (meow-define-keys 'leader `("p" . ("project" . ,project-prefix-map)))
 
   ;; Some useful helper functions.
   (defun project-make-relative-to-root (filename &optional project)
     "Convert FILENAME to be relative to the root of a `project.el' PROJECT.  Defaults to `project-current'"
     (file-relative-name filename (project-root (or project (project-current))))))
+
+(use-package disproject
+  :straight
+  '(disproject
+    :type git
+    :host github
+    :repo "aurtzy/disproject")
+  :demand t
+  :config
+  (meow-define-keys 'leader `("p" . ("project" . disproject-dispatch))))
 
 (provide 'editor/project)
 ;;; project.el ends here
