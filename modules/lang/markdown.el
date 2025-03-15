26ca67a51402fcb0cd03891cc240666bb90f0268
Reed Mullanix <reedmullanix@gmail.com>
2025-03-15 10:06:03 -0400

    feat: spell-fu in markdown mode

--- modules/lang/markdown.el
+++ modules/lang/markdown.el
@@ -5,7 +5,19 @@
 ;;; Code:
 
 (use-package markdown-mode
-  :straight nil)
+  :straight nil
+  :custom
+  (markdown-enable-math t)
+  )
+
+(use-package spell-fu
+  :commands
+  (spell-fu-mode)
+  :preface
+  (defun spell-fu-markdown-hook ()
+    (setq spell-fu-faces-exclude '(markdown-code-face markdown-comment-face markdown-math-face))
+    (spell-fu-mode 1))
+  :hook (markdown-mode . spell-fu-markdown-hook))
 
 (provide 'lang/markdown)
 ;;; markdown.el ends here
