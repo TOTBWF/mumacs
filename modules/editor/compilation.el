54c7406508558c78f91063d944f6a3184cfa8f14
Reed Mullanix <reedmullanix@gmail.com>
2025-04-21 15:28:23 -0400

    chore: optimize `compile' to use a pipe instead of a pty

--- modules/editor/compilation.el
+++ modules/editor/compilation.el
@@ -6,9 +6,22 @@
 
 ;; Replace ANSI color escape codes with faces.
 (use-package ansi-color
+  :hook
+  (compilation-filter-hook . ansi-color-compilation-filter)
   :custom
-  (ansi-color-for-compilation-mode t)
-  :hook (compilation-filter . ansi-color-compilation-filter))
+  (ansi-color-for-compilation-mode t))
+
+(use-package compile
+  :straight nil
+  :preface
+  (defun compilation-start--use-pipe (fn &rest args)
+    "Advice to ensure that `compilation-start' uses a pipe rather than a pty
+for the compilation command. This increases performance on OSX by a factor of 10,
+as the default pty size is a pitiful 1024 bytes."
+    (let ((process-connection-type nil))
+      (apply fn args)))
+  :advice
+  (compilation-start :around compilation-start--use-pipe))
 
 (provide 'editor/compilation)
 ;;; compilation.el ends here
