b89cfef0f659400dc63b3a6413929e090d75493c
Reed Mullanix <reedmullanix@gmail.com>
2025-05-27 10:19:52 -0700

    chore: shorter doc-string on compilation start advice

--- modules/editor/compilation.el
+++ modules/editor/compilation.el
@@ -15,14 +15,15 @@
 (use-package compile
   :ensure nil
   :preface
-  (defun compilation-start--use-pipe (fn &rest args)
-    "Advice to ensure that `compilation-start' uses a pipe rather than a pty
-for the compilation command. This increases performance on OSX by a factor of 10,
-as the default pty size is a pitiful 1024 bytes."
+  (defun compilation-start@use-pipe (fn &rest args)
+    ;; checkdoc-params: (fn args)
+    "Advice to ensure that `compilation-start' uses a pipe rather than
+a pty for the compilation command. This increases performance on OSX
+by a factor of 10, as the default pty size is a pitiful 1024 bytes."
     (let ((process-connection-type nil))
       (apply fn args)))
   :advice
-  (compilation-start :around compilation-start--use-pipe))
+  (compilation-start :around compilation-start@use-pipe))
 
 (provide 'editor/compilation)
 ;;; compilation.el ends here
