cba5032b13252c602022871372ab7f889755e940
Reed Mullanix <reedmullanix@gmail.com>
2025-05-27 08:56:22 -0700

    feat: flymake keybindings

--- modules/editor/flymake.el
+++ modules/editor/flymake.el
@@ -4,8 +4,17 @@
 
 ;;; Code:
 
+(require 'core/meow)
+
+(define-leader meow-flymake-leader-map "e" "error")
+
 (use-package flymake
-  :ensure nil)
+  :ensure nil
+  :bind
+  (:map meow-flymake-leader-map
+	("e" . flymake-show-buffer-diagnostics)
+	("j" . flymake-goto-next-error)
+	("k" . flymake-goto-prev-error)))
 
 (provide 'editor/flymake)
 ;;; flymake.el ends here
