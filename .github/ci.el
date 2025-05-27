f592ebb9ac20a020f6e9dabb6ab37763cf0cc629
Reed Mullanix <reedmullanix@gmail.com>
2025-05-26 23:22:09 -0700

    chore: wrap load inside of group for gh actions

--- .github/ci.el
+++ .github/ci.el
@@ -48,8 +48,12 @@ Note that this will create a buffer that visits the source of the diagnostic."
   (setq command-error-function #'ci-log-emacs-error)
   (setq byte-compile-log-warning-function #'ci-log-byte-compile-warning)
   (setq user-emacs-directory default-directory)
+  (message "::group::Loading Emacs")
   (load (concat user-emacs-directory "early-init.el"))
-  (load (concat user-emacs-directory "init.el")))
+  (load (concat user-emacs-directory "init.el"))
+  ;; Make sure to wait until package loading is done.
+  (elpaca-wait)
+  (message "::endgroup::"))
 
 
 (provide 'ci.el)
