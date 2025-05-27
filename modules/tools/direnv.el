d34c6ae9efaed42ca79333ade77b7cec6cd85e49
Reed Mullanix <reedmullanix@gmail.com>
2025-05-27 11:31:05 -0700

    fix: fix direnv

--- modules/tools/direnv.el
+++ modules/tools/direnv.el
@@ -7,8 +7,8 @@
 
 (use-package envrc
   :ensure t
-  :config
-  (after-elpaca-init-hook . envrc-global-mode))
+  :hook
+  (elpaca-after-init-hook . envrc-global-mode))
 
 (provide 'tools/direnv)
 ;;; direnv.el ends here
