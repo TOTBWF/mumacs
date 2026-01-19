124b5b0d97bff1953f32f60018f1b7ab01ce5e4c
Reed Mullanix <reedmullanix@gmail.com>
2026-01-19 15:34:56 -0500

    fix: idris mode on .idr files

--- modules/lang/idris.el
+++ modules/lang/idris.el
@@ -4,7 +4,8 @@
 
 ;;; Code:
 (use-package idris2-mode
-  :ensure (idris2-mode :type git :host github :repo "idris-community/idris2-mode"))
+  :ensure (idris2-mode :type git :host github :repo "idris-community/idris2-mode")
+  :mode "\\.idr\\'")
 
 (provide 'lang/idris)
 ;;; idris.el ends here
