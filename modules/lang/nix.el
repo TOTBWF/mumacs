0ac608a2cb7e5fc3fcb6115d70b37b1827478028
Reed Mullanix <reedmullanix@gmail.com>
2026-01-19 15:35:43 -0500

    feat: add nixd eglot server for nix-mode

--- modules/lang/nix.el
+++ modules/lang/nix.el
@@ -1,11 +1,14 @@
 ;;; lang/nix ---  -*- lexical-binding: t; -*-
 
 ;;; Commentary:
+(require 'editor/lsp)
 
 ;;; Code:
 (use-package nix-mode
   :ensure t
-  :commands nix-mode)
+  :commands nix-mode
+  :eglot
+  (nix-mode . '("nixd")))
 
 (provide 'lang/nix)
 ;;; nix.el ends here
